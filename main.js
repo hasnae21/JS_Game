6ec70ef6182fdaa28ba1c39bb7d6d99005d64716
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2021-12-08 10:01:59 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,57 +2,70 @@
         var randomNumber= Math.floor(Math.random() * 100)+1;
         console.log(randomNumber);
 // DOM
-const divBonde = document.querySelector('.bonde');
 const message = document.getElementById('message');
 const form= document.getElementById('input_nbr');
 const input= document.getElementById('number');
 const btnplay = document.getElementById('btnplay');
 const btnreplay = document.getElementById('btnreplay');
 const label = document.getElementById('label');
+let tries=document.getElementById('attempts');
 let counter = document.querySelector('.counte');
+let GifTW = document.querySelector('.imageTW');
+let GifW = document.querySelector('.imageW');
+let GifL = document.querySelector('.imageL');
 
-// Initialiser mon compteur de tentatives i
-let i = 10;
+// Initialiser mes compteurs de tentatives i AND j
+let j = 1;
+let i = 9;
+i= 10-j; 
 // Game one
-
 const play = () => {
     form.addEventListener('submit', (e) => {
         e.preventDefault();
         const val_Input = parseInt(input.value);
-        if ( val_Input === randomNumber && i<=2  && i<11){
-                message.textContent = "Well done, you are a genius !!!";
+        if ( val_Input === randomNumber && j<3 ){
+                message.innerHTML = `Well done, you are a genius !!!`;
                 btnreplay.style.display = "block";
                 btnplay.style.display = "none";
                 label.style.display= "none";
-        }else if(val_Input === randomNumber && i>2 && i<11){
-                message.textContent = `Congratulations, you won after ${10-i} tries`;
+                tries.style.display= "none";
+                input.style.display= "none";
+                GifTW.style.display = "block";
+        }else if(val_Input === randomNumber && j>2 ){
+                message.innerHTML = `Congratulations,<br> you won after ${j} attempts`;
                 btnreplay.style.display = "block";
                 btnplay.style.display = "none";
                 label.style.display= "none";
+                tries.style.display= "none";
+                input.style.display= "none";
+                GifW.style.display = "block";
         }
         if( val_Input !== randomNumber){
-            if(val_Input < randomNumber && i<11){
-                message.textContent = `the number is smaller, choose a higher one`;
+            if(val_Input < randomNumber ){
+                message.textContent = `The number is smaller, choose a higher one`;
                 btnreplay.style.display = "none";
-
-            }else if(val_Input > randomNumber  && i<11){
-                message.textContent = `the number is larger, choose a smaller one`;
+                tries.style.display= "flex";
+            }else if(val_Input > randomNumber  ){
+                message.textContent = `The number is larger, choose a smaller one`;
                 btnreplay.style.display = "none";
-
+                tries.style.display= "flex";
             }
-            i--;
             verifyloose();  
         }
-        counter.innerHTML=i;  
+        i--;
+        j++;
+        counter.innerHTML= i+1;  
     })
     const verifyloose = () => {
-        if (i === 0) { 
+        if (j === 10) { 
             btnplay.setAttribute("disabled", "");
-            message.textContent = `Game over. the answer was
-             ${randomNumber}`;
+            message.innerHTML = `Game over.<br/> the answer was ${randomNumber}`;
             btnplay.style.display = "none";
             btnreplay.style.display = "block";
             label.style.display= "none";
+            input.style.display= "none";
+            tries.style.display= "none";
+            GifL.style.display = "block";
         }
     }
     btnreplay.addEventListener('click',() =>{
